b9fa074e314607f303b08e8cdeb089ed94a6f917
Gabriel Trujillo Gómez <GaboGomez@imac-de-gabriel-trujillo-gomez.local>
2017-04-20 00:47:18 -0500

    Initial Commit

---
+++ PicFix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PicFix
+//
+//  Created by Gabriel Trujillo Gómez on 4/20/17.
+//  Copyright © 2017 PicFix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
