d456bf7f4d41cf33415096f83b32d820e8dcd133
Gabriel Trujillo Gómez <GaboGomez@imac-de-gabriel-trujillo-gomez.local>
2017-04-20 01:28:16 -0500

    First login layout AND main layout

--- PicFix/ViewController.swift
+++ PicFix/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated. sdfsdf
+        // Dispose of any resources that can be recreated. sdfsdf ssss
     }
 
 
